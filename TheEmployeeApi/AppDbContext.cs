b93152463911c9031ed1e7ec3f85f37b289b0c2f
nino kristesiashvili <kristesiashvilinino@gmail.com>
2025-03-23 16:27:55 +0400

    Add AppDbContext for Entity Framework Core; configure Employees DbSet and include SQLite package

---
+++ TheEmployeeApi/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TheEmployeeApi.Employees;
+
+namespace TheEmployeeApi
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
